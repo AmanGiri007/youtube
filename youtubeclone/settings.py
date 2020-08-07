91bd6183370145d4c994dcaaaa89497076069ad9
AmanGiri007 <giriaman437@gmail.com>
2020-08-07 17:45:18 +0545

    change

--- youtubeclone/settings.py
+++ youtubeclone/settings.py
@@ -118,12 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS=(
+STATIC_URL = 'whitetube/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+'''STATICFILES_DIRS=(
     os.path.join(BASE_DIR,'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+)'''
 
 db_from_env=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
