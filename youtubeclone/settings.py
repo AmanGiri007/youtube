324ab40aec25fd474b33a1f5ab80f85f29f3ffbb
AmanGiri007 <giriaman437@gmail.com>
2020-08-07 19:08:09 +0545

    change

--- youtubeclone/settings.py
+++ youtubeclone/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 db_from_env=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 django_heroku.settings(locals())
