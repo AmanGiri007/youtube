1b10d3676cfe3d3fa38bd163dd8e570a223b7c20
AmanGiri007 <giriaman437@gmail.com>
2020-08-07 11:03:35 +0545

    changes made

--- youtubeclone/settings.py
+++ youtubeclone/settings.py
@@ -121,8 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILESDIR=[
-    os.path.join(BASE_DIR,'static'),
-]
+STATIC_ROOT=os.path.join(BASE_DIR,'static'),
+
 db_from_env=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
